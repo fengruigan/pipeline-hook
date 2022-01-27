268a5de558957876a2e019bcc39cb69799221b38
Fengrui Gan <fengruigan@gmail.com>
2022-01-26 22:22:54 -0800

    prep hook reroute

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require("express"),
-    app = express();
+    app = express(),
+    axios = require("axios").default;
 
 app.get("/", (req, res) => {
     res.send("hi");
 });
+
+// app.post("/", (req, res) => {
+//     console.log(req.body);
+//     let msg = {};
+//     msg.content = req.body;
+//     axios.fetch(DISCORD_HOOK, {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: msg,
+//     });
+// });
 
 app.listen(process.env.PORT || 8000, () => {
     console.log("server up");
