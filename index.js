e91a2abb3812fcd50a5490cfd1493a1f1d9d1a46
Fengrui Gan <fengruigan@gmail.com>
2022-01-26 23:07:34 -0800

    reroute entire post body

--- index.js
+++ index.js
@@ -2,22 +2,20 @@ const express = require("express"),
     app = express(),
     axios = require("axios").default;
 
+app.use(express.json());
+require("dotenv").config();
+
 app.get("/", (req, res) => {
     res.send("hi");
 });
 
-// app.post("/", (req, res) => {
-//     console.log(req.body);
-//     let msg = {};
-//     msg.content = req.body;
-//     axios.fetch(DISCORD_HOOK, {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//         },
-//         body: msg,
-//     });
-// });
+app.post("/", (req, res) => {
+    console.log(req.body);
+    let msg = {};
+    msg.content = JSON.stringify(req.body);
+    axios.post(process.env.DISCORD_HOOK, msg);
+    res.send("OK");
+});
 
 app.listen(process.env.PORT || 8000, () => {
     console.log("server up");
